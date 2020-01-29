e1f74938da6abc14e9e9c835be8cca1ad8eebacb
jonathanbrierre <jonathanbrierre@yahoo.com>
2020-01-29 09:34:00 -0500

    Done.

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,14 @@
 // Code ClickityClick Component Here
+import React, { Component } from 'react'
+
+export class ClickityClick extends Component {
+    render() {
+        return (
+            <div>
+                hello
+            </div>
+        )
+    }
+}
+
+export default ClickityClick
